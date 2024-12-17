2c4cd215b0ebbed378702ed5eaad3fb85f744192
zim514 <krisyoung@gmail.com>
2024-12-17 14:01:42 -0500

    Revert manifest.json change

--- custom_components/ha_blueair/humidifier.py
+++ custom_components/ha_blueair/humidifier.py
@@ -14,38 +14,23 @@ from homeassistant.components.humidifier import (
 )
 
 from . import DOMAIN
-from .blueair_update_coordinator_device_aws import BlueairUpdateCoordinatorDeviceAws
+from .blueair_update_coordinator_device_aws import BlueairUpdateCoordinator
 from .const import DATA_AWS_DEVICES, MODE_WICK_DRY
-from .entity import BlueairEntity
+from .entity import BlueairEntity, async_setup_entry_helper
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up the Blueair humidifier from config entry, but only if the device type name is Humidifier. I'm not sure if we should look for the specific model, as blueair might add new types of humidifiers in the future"""
+    """Set up the Blueair humidifier from config entry"""
 
-    data = hass.data[DOMAIN]
-    aws_devices = data[DATA_AWS_DEVICES]
-
-    humidifier_devices = []
-
-    for device in aws_devices:
-        api_device = device.blueair_api_device
-        _LOGGER.debug(
-            f"Found device: name={api_device.name}, "
-            f"type_name={api_device.type_name}, "
-            f"sku={api_device.sku}"
-        )
-
-        if api_device.type_name == "Humidifier":
-            humidifier_devices.append(device)
-
-    if not humidifier_devices:
-        _LOGGER.debug("No humidifier devices found")
-        return
-
-    async_add_entities(
-        BlueairAwsHumidifier(coordinator) for coordinator in humidifier_devices
+    async_setup_entry_helper(
+        hass,
+        config_entry,
+        async_add_entities,
+        entity_classes=[
+            BlueairAwsHumidifier,
+        ],
     )
 
 
@@ -54,17 +39,12 @@ class BlueairAwsHumidifier(BlueairEntity, HumidifierEntity):
 
     @classmethod
     def is_implemented(kls, coordinator):
-        return isinstance(coordinator, BlueairUpdateCoordinatorDeviceAws)
+        return coordinator.target_humidity is not NotImplemented
 
-    def __init__(self, coordinator: BlueairUpdateCoordinatorDeviceAws):
+    def __init__(self, coordinator: BlueairUpdateCoordinator):
         """Initialize the humidifer."""
         super().__init__("Humidifier", coordinator)
         return None
-
-    @property
-    def should_poll(self) -> bool:
-        """No polling needed with coordinator."""
-        return False
 
     @property
     def available(self) -> bool:
@@ -78,9 +58,6 @@ class BlueairAwsHumidifier(BlueairEntity, HumidifierEntity):
     @property
     def supported_features(self) -> int:
         return HumidifierEntityFeature.MODES
-        # if self.coordinator.fan_speed is not NotImplemented:
-        # features |= HumidifierEntityFeature.SET_SPEED
-        # return features
 
     @property
     def available_modes(self):
@@ -110,8 +87,6 @@ class BlueairAwsHumidifier(BlueairEntity, HumidifierEntity):
 
     @property
     def current_humidity(self):
-        if self.coordinator.humidity is None:
-            return None
         return self.coordinator.humidity
 
     async def async_turn_off(self, **kwargs: any) -> None:
@@ -129,17 +104,25 @@ class BlueairAwsHumidifier(BlueairEntity, HumidifierEntity):
 
     async def async_set_mode(self, mode):
         if mode == MODE_AUTO:
+            # This mode doesn't apply when off
             await self.coordinator.set_fan_auto_mode(True)
+            await self.coordinator.set_running(True)
             self.async_write_ha_state()
         elif mode == MODE_WICK_DRY:
+            # This mode doesn't apply when on
             await self.coordinator.set_wick_dry_mode(True)
+            await self.coordinator.set_running(False)
             self.async_write_ha_state()
         elif mode == MODE_SLEEP:
+            # This mode doesn't apply when off
             await self.coordinator.set_night_mode(True)
+            await self.coordinator.set_fan_auto_mode(False)
+            await self.coordinator.set_running(False)
             self.async_write_ha_state()
         elif mode == MODE_NORMAL:
+            # This mode doesn't apply when off
             await self.coordinator.set_fan_auto_mode(False)
-            await self.coordinator.set_night_mode(True)
+            await self.coordinator.set_night_mode(False)
             await self.coordinator.set_running(True)
 
             self.async_write_ha_state()
