958866d3fe057893e0d9a99c4facb5428d61419e
Bren Dahl <dahl.brendan@gmail.com>
2025-01-14 16:56:02 -0500

    chore: typo

--- custom_components/ha_blueair/humidifier.py
+++ custom_components/ha_blueair/humidifier.py
@@ -40,7 +40,7 @@ class BlueairAwsHumidifier(BlueairEntity, HumidifierEntity):
         return coordinator.auto_regulated_humidity is not NotImplemented
 
     def __init__(self, coordinator: BlueairUpdateCoordinator):
-        """Initialize the humidifer."""
+        """Initialize the humidifier."""
         self._attr_device_class = HumidifierDeviceClass.HUMIDIFIER
         self._attr_supported_features = HumidifierEntityFeature.MODES
         self._attr_available_modes = [
